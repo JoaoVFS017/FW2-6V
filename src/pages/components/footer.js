d3acd484f79031dc741cc4fb9040d336c2c5217e
JoaoVFS017 <119964116+JoaoVFS017@users.noreply.github.com>
2024-10-15 18:37:04 +0000

    aula 14/10

--- src/pages/components/footer.js
+++ src/pages/components/footer.js
@@ -1,7 +1,8 @@
 import {Container} from "react-bootstrap"
+import style from "@/styles/Footer.module.css"
 export default function Footer() {
     return <>
-    <Container className="position-button bottom-0 start-50">
+    <Container className={`${style.degradeFooter}position-button bottom-0 start-50`}>
         <footer className="px-3 text-light bg-dark rounded">
             <ul className="nav justify-content-center border-bottom pb-1 mb-2 ">
                 <li className="nav-item">Disciplina de FrameWorks2</li>
